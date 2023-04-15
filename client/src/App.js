a4dc81b21c45df5ce88b58fecee847f05f26451c
Abhishek venkata <abhishekvenkata.199715@gmail.com>
2023-04-15 05:28:55 -0400

    Added components and check to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./pages/register";
-import PageRender from "./PageRender";
+import PageRender from './customRouter/PageRender'
+import PrivateRouter from './customRouter/privateRouter';
 import Home from "./pages/home";
 import Login from "./pages/login";
+import Register from './pages/register'
 
 import { refreshToken } from "./redux/actions/authActions";
 import Alert from "./components/alert/Alert";
 import { useSelector, useDispatch } from "react-redux";
 import {} from "./redux/actions/authActions";
+
+import Alert from './components/alert/Alert'
+import Header from './components/header/header'
 
 function App() {
   const { auth } = useSelector((state) => state);
@@ -26,15 +31,13 @@ function App() {
         <div className="main">
           <Router>
             <Alert />
+            {auth.token && <Header/>}
             <Routes>
-              <Route
-                exact
-                path="/"
-                element={auth.token ? <Home /> : <Login />}
-              />
-              <Route exact path="/Login" element={<Login />} />
-              <Route exact path="/:page" element={<PageRender />} />
-              <Route exact path="/:page/:id" element={<PageRender />} />
+              <Route exact path='/' element={auth.token ? <Home /> : <Login />} />
+              <Route exact path='/register' element = {<Register/>} />
+              <Route exact path='/Login' element={<Login />} />
+              <Route exact path='/:page' element={<PrivateRouter><PageRender /></PrivateRouter>}/>
+              <Route exact path='/:page/:id' element={<PrivateRouter><PageRender /></PrivateRouter>}/>
             </Routes>
           </Router>
         </div>

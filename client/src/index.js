0dddc5ae94db9831a1b0f95bedcbb65251d397ed
Abhishek venkata <abhishekvenkata.199715@gmail.com>
2023-04-15 12:55:55 -0400

    Add login and changes to App.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import './../src/styles/global.css'
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import DataProvider from "./redux/store";

77c480a8f6f14cc7ed00927be905d50982b746f7
Sreya-Vallabhaneni <114380178+Sreya-Vallabhaneni@users.noreply.github.com>
2023-04-14 22:12:51 -0400

    App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,45 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Register from './pages/register'
-import PageRender from './PageRender'
-import Home from './pages/home'
-import Login from './pages/login'
+import Register from "./pages/register";
+import PageRender from "./PageRender";
+import Home from "./pages/home";
+import Login from "./pages/login";
+
+import { refreshToken } from "./redux/actions/authActions";
+import Alert from "./components/alert/Alert";
+import { useSelector, useDispatch } from "react-redux";
+import {} from "./redux/actions/authActions";
 
 function App() {
+  const { auth } = useSelector((state) => state);
+  console.log("auth token in Login Component:", auth.token);
+  const dispatch = useDispatch();
+  console.log("AUTH TOKEN:" + auth.token);
+  useEffect(() => {
+    dispatch(refreshToken());
+  }, [dispatch]);
+
   return (
     <>
       <input type="checkbox" id="theme" />
       <div className="App">
         <div className="main">
           <Router>
-          <Routes>
-            <Route exact path ='/' element={<Login/>}/>
-            <Route exact path ='/:page' element={<PageRender/>}/>
-            <Route exact path = "/:page/:id" element={<PageRender/>}/>
-          </Routes>
+            <Alert />
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={auth.token ? <Home /> : <Login />}
+              />
+              <Route exact path="/Login" element={<Login />} />
+              <Route exact path="/:page" element={<PageRender />} />
+              <Route exact path="/:page/:id" element={<PageRender />} />
+            </Routes>
           </Router>
         </div>
       </div>
-
-      </>
+    </>
   );
 }
-
 export default App;

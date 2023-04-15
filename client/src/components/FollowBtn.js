da24245b861b001b1e1855781bad80e27d93f5fb
Kasavaraju-v <114193191+Kasavaraju-v@users.noreply.github.com>
2023-04-15 17:58:09 -0400

    Added components for follow flow

--- client/src/components/FollowBtn.js
+++ client/src/components/FollowBtn.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {follow, unfollow} from '../redux/actions/profileAction'
 
-const FollowBtn = () => {
+
+
+const FollowBtn = ({user}) => {
+
+    const [followed, setFollowed] = useState(false)
+
+    const {auth, profile} = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    const [load, setLoad ] = useState(false)
+
+    useEffect(() => {
+        if(auth.user.following.find(item => item._id === user._id)){
+            setFollowed(true)
+        }
+    },[auth.user.following, user._id])
+
+    const handleFollow = async () => {
+        if(load) return;
+        setFollowed(true)
+        setLoad(true)
+        await dispatch(follow({users: profile.users, user, auth}))
+        setLoad(false)
+    }
+
+    const handleUnFollow = async () => {
+        if(load) return
+        setFollowed(false)
+        setLoad(true)
+        dispatch(unfollow({users: profile.users, user, auth}))
+        setLoad(false)
+    }
+
+    console.log({user})
     return (
-        <button className='btn btn-outline-info'>
-            Follow
-        </button>
-    )
-}
+        <>
+            {
+                followed
+                    ? <button className='btn btn-outline-danger'
+                              onClick={handleUnFollow}>Unfollow</button>
+                    :
+                    <button className='btn btn-outline-info'
+                            onClick={handleFollow}> Follow</button>
+            }
 
-export default FollowBtn
+        </>
+    )
+
+}
--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -3,16 +3,26 @@ import Avatar from './Avatar'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-const UserCard = ({ user, border}) => {
+const UserCard = ({children, user, border, handleClose,setShowFollowers, setShowFollowing,}) => {
+
+    const handleCloseAll = () => {
+        if(handleClose) handleClose()
+        if(setShowFollowers) setShowFollowers(false)
+        if(setShowFollowing) setShowFollowing(false)
+    }
 
     return (
         <div className={`d-flex p-2 align-items-center justify-content-between w-100 ${border}`}>
             <div>
-                <Avatar src={user.avatar} size="big-avatar" />
-                <div className="ml-1" style={{transform: 'translateY(-2px)'}}>
-                    <span className="d-block">{user.username}</span>
-                </div>
+                <Link to={`/profile/${user._id}`} onClick={handleCloseAll}
+                      className="d-flex align-items-center">
+                    <Avatar src={user.avatar} size="big-avatar" />
+                    <div className="ml-1" style={{transform: 'translateY(-2px)'}}>
+                        <span className="d-block">{user.username}</span>
+                    </div>
+                </Link>
             </div>
+            {children}
         </div>
     )
 }

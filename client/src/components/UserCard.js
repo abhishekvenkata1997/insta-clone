b93d8891aa6fbca37b44e7d0d1b394f709bbf95a
Abhishek venkata <abhishekvenkata.199715@gmail.com>
2023-04-15 05:31:28 -0400

    Added components FollowBtn and UserCard

---
+++ client/src/components/UserCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Avatar from './Avatar'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+const UserCard = ({ user, border}) => {
+
+    return (
+        <div className={`d-flex p-2 align-items-center justify-content-between w-100 ${border}`}>
+            <div>
+                <Avatar src={user.avatar} size="big-avatar" />
+                <div className="ml-1" style={{transform: 'translateY(-2px)'}}>
+                    <span className="d-block">{user.username}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserCard

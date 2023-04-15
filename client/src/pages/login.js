04cd7a75c229cc9edf4e96ab04d11a92524140bf
Sreya-Vallabhaneni <114380178+Sreya-Vallabhaneni@users.noreply.github.com>
2023-04-14 22:47:36 -0400

    Login page

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,13 +1,86 @@
-import {React, useState} from 'react'
-import {Link, useRouteLoaderData } from 'react-router-dom'
+import { React, useState } from "react";
+//import {Link, useRouteLoaderData } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import {} from "../redux/actions/authActions";
+import { login } from "../redux/actions/authActions";
+import { useDispatch } from "react-redux";
 
 const Login = () => {
+  const initialState = { email: "", password: "" };
+  const [userData, setUserData] = useState(initialState);
+  const { email, password } = userData;
 
-    return (
-        <div>
-            Login!
+  const [typePass, setTypePass] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userData);
+    dispatch(login(userData));
+  };
+  return (
+    <div className="auth_page">
+      <form onSubmit={handleSubmit}>
+        <h3 className="text-uppercase text-center mb-4">V-Network</h3>
+
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            name="email"
+            aria-describedby="emailHelp"
+            onChange={handleChangeInput}
+            value={email}
+          />
+
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
         </div>
-    )
-}
 
-export default Login
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+
+          <div className="pass">
+            <input
+              type={typePass ? "text" : "password"}
+              className="form-control"
+              id="exampleInputPassword1"
+              onChange={handleChangeInput}
+              value={password}
+              name="password"
+            />
+            <small onClick={() => setTypePass(!typePass)}>
+              {typePass ? "Hide" : "Show"}
+            </small>
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-dark w-100"
+          disabled={email && password ? false : true}
+        >
+          Login
+        </button>
+
+        <p className="my-2">
+          You don't have an account?{" "}
+          <Link to="/register" style={{ color: "crimson" }}>
+            Register Now
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;

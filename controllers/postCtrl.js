85170412af5aeb63dcca83723b8301d1e058bb48
anirudhsk2107 <sampathkumar.an@northeastern.edu>
2023-04-16 08:18:49 -0400

    Like and Unlike posts

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -1,3 +1,4 @@
+const { populate } = require('../models/userModel')
 const Posts = require('./../models/postModel')
 
 const postCtrl = {
@@ -33,7 +34,13 @@ const postCtrl = {
                 user: [...req.user.following, req.user._id]
             }).sort('-createdAt')
             .populate("user likes","avatar username fullname")
-
+            .populate({
+                path:"comments",
+                populate:{
+                    path: "user likes",
+                    select:"-password"
+                }
+            })
             res.json({
                 msg: 'Success!',
                 result: posts.length,
@@ -60,6 +67,39 @@ const postCtrl = {
                     content, images
                 }
             })
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+
+    likePost: async (req, res) => {
+        try {
+            const post = await Posts.find({_id: req.params.id, likes: req.user._id})
+            if(post.length > 0) return res.status(400).json({msg: "You liked this post."})
+
+            const like = await Posts.findOneAndUpdate({_id: req.params.id}, {
+                $push: {likes: req.user._id}
+            }, {new: true})
+
+            if(!like) return res.status(400).json({msg: 'This post does not exist.'})
+
+            res.json({msg: 'Liked Post!'})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    unLikePost: async (req, res) => {
+        try {
+
+            const like = await Posts.findOneAndUpdate({_id: req.params.id}, {
+                $pull: {likes: req.user._id}
+            }, {new: true})
+
+            if(!like) return res.status(400).json({msg: 'This post does not exist.'})
+
+            res.json({msg: 'UnLiked Post!'})
+
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }

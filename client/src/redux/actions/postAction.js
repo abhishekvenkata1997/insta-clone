a43c2472c7cba0424fdb1c8a6a9906868558f3d4
anirudhsk2107 <sampathkumar.an@northeastern.edu>
2023-04-16 02:23:07 -0400

    Style update & post code changes

--- client/src/redux/actions/postAction.js
+++ client/src/redux/actions/postAction.js
@@ -1,13 +1,17 @@
+
 import { GLOBALTYPES } from './globalTypes'
 import { imageUpload } from '../../utils/imageUpload'
-import { postDataAPI } from '../../utils/fetchData'
+import { postDataAPI, getDataAPI, patchDataAPI } from '../../utils/fetchData'
 
 export const POST_TYPES = {
-    CREATE_POST: 'CREATE_POST'
+    CREATE_POST: 'CREATE_POST',
+    LOADING_POST: 'LOADING_POST',
+    GET_POSTS: 'GET_POSTS',
+    UPDATE_POST: 'UPDATE_POST'
 }
 
 export const createPost = ({content, images, auth}) => async (dispatch) => {
-
+    
     let media = []
     try{
         dispatch({
@@ -19,18 +23,71 @@ export const createPost = ({content, images, auth}) => async (dispatch) => {
             media = await imageUpload(images)
         }
         const res = await postDataAPI('posts', {content, images: media}, auth.token)
-        //console.log(res)
-        // console.log(media)
-        dispatch({
-            type: POST_TYPES.CREATE_POST,
-            payload: res.data.newPost
-        })
+       dispatch({
+        type: POST_TYPES.CREATE_POST, 
+        payload: {...res.data.newPost, user: auth.user}
+       })
         dispatch({
             type: GLOBALTYPES.ALERT,
             payload: {loading: false}
         })
     }
     catch(err){
+        dispatch({
+            type: GLOBALTYPES.ALERT,
+            payload: {error: err.response.data.msg}
+        })
+    }
+}
+
+export const getPosts = (token) => async(dispatch) => {
+    try{
+        dispatch({
+            type: POST_TYPES.LOADING_POST,
+            payload: true
+        })
+        const res = await getDataAPI('posts',token)
+        
+        dispatch({
+            type: POST_TYPES.GET_POSTS,
+            payload: {...res.data, page: 2}
+        })
+
+        dispatch({
+            type: POST_TYPES.LOADING_POST,
+            payload: false
+        })
+    }
+    catch(err) {
+        dispatch({
+            type: GLOBALTYPES.ALERT,
+            payload: {error: err.response.data.msg}
+        })
+    }
+}
+
+export const updatePost = ({content, images, auth, status}) => async (dispatch) => {
+    let media = []
+    const imgNewUrl = images.filter(img => !img.url)
+    const imgOldUrl = images.filter(img => img.url)
+
+    if(status.content === content 
+        && imgNewUrl.length === 0
+        && imgOldUrl.length === status.images.length
+    ) return;
+
+    try {
+        dispatch({ type: GLOBALTYPES.ALERT, payload: {loading: true} })
+        if(imgNewUrl.length > 0) media = await imageUpload(imgNewUrl)
+
+        const res = await patchDataAPI(`post/${status._id}`, { 
+            content, images: [...imgOldUrl, ...media] 
+        }, auth.token)
+
+        dispatch({ type: POST_TYPES.UPDATE_POST, payload: res.data.newPost })
+
+        dispatch({ type: GLOBALTYPES.ALERT, payload: {success: res.data.msg} })
+    } catch (err) {
         dispatch({
             type: GLOBALTYPES.ALERT,
             payload: {error: err.response.data.msg}
--- client/src/redux/reducers/postReducer.js
+++ client/src/redux/reducers/postReducer.js
@@ -1,9 +1,10 @@
 import { POST_TYPES } from './../actions/postAction'
-
+import { EditData } from '../actions/globalTypes'; 
 const initialState = {
+    loading: false,
     posts: [],
     result: 0,
-    page: 0
+    page: 2
 }
 
 const postReducer = (state = initialState, action) => {
@@ -12,7 +13,24 @@ const postReducer = (state = initialState, action) => {
         case POST_TYPES.CREATE_POST:
             return {
                 ...state,
-                posts: [...state.posts, action.payload]
+                posts: [action.payload, ...state.posts]
+            };
+
+        case POST_TYPES.LOADING_POST:
+            return {
+                ...state,
+                loading: action.payload
+            };
+        case POST_TYPES.GET_POSTS:
+            return {
+                ...state,
+                posts: action.payload.posts,
+                result: action.payload.result
+            };
+        case POST_TYPES.UPDATE_POST:
+            return {
+                ...state,
+                posts: EditData(state.posts, action.payload._id, action.payload)
             };
         default:
             return state;

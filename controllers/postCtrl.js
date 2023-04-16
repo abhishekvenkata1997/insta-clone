9ceeecc3ea2373f278951fba218f2d838915e456
anirudhsk2107 <sampathkumar.an@northeastern.edu>
2023-04-15 23:40:31 -0400

    postCtrl & postRoute Updated

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -8,7 +8,7 @@ const postCtrl = {
             if(images.length === 0)
             {
                 return res.status(400).json({msg: "Please add your photos"})
-            }
+            } 
             const newPost = new Posts(
                 {
                     content, images, user: req.user._id
@@ -24,7 +24,48 @@ const postCtrl = {
         catch(err) {
             return res.status(500).json({msg: err.message})
         }
-    }
+    },
+
+    getPosts: async(req,res) => {
+        try{
+            console.log(req.user)
+            const posts = await Posts.find({
+                user: [...req.user.following, req.user._id]
+            }).sort('-createdAt')
+            .populate("user likes","avatar username fullname")
+
+            res.json({
+                msg: 'Success!',
+                result: posts.length,
+                posts
+            })
+        }
+        catch(err){
+            res.status(500).json({msg: err.message})
+        }
+    },
+
+    updatePost: async (req, res) => {
+        try {
+            const { content, images } = req.body
+
+            const post = await Posts.findOneAndUpdate({_id: req.params.id}, {
+                content, images
+            }).populate("user likes", "avatar username fullname")
+
+            res.json({
+                msg: "Updated Post!",
+                newPost: {
+                    ...post._doc,
+                    content, images
+                }
+            })
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+
+
 }
 
 module.exports = postCtrl
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,5 +3,9 @@ const postCtrl = require('./../controllers/postCtrl')
 const auth = require('./../middleware/auth')
 
 router.route('/posts')
-    .post(auth, postCtrl.createPost)
+        .post(auth, postCtrl.createPost)
+        .get(auth, postCtrl.getPosts)
+
+router.route('/post/:id')
+        .patch(auth, postCtrl.updatePost)
 module.exports = router

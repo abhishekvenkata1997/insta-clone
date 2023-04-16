72edba16e01fbad73e730201dcab8df5d0b00052
Devki Patel <74588709+Devkip8112@users.noreply.github.com>
2023-04-16 16:55:50 -0400

    ab

--- client/src/pages/profile/[id].js
+++ client/src/pages/profile/[id].js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Info from './../../components/profile/info'
 import Posts from './../../components/profile/posts'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import LoadIcon from './../../images/loading.gif'
+import { getProfileUsers } from "../../redux/actions/profileAction";
+import {useParams} from 'react-router-dom'
 
 const Profile = () => {
-    const { profile } = useSelector(state => state)
+    const { profile, auth } = useSelector(state => state)
+
+    const dispatch = useDispatch()
+    const { id } = useParams()
+
+    useEffect(() => {
+        if(profile.ids.every(item => item !== id)){
+            dispatch(getProfileUsers({id, auth}))
+        }
+    },[id, auth, dispatch, profile.ids])
+
     return (
         <div className="profile">
+            <Info auth={auth} profile={profile} dispatch={dispatch} id={id}/>
+
             {
                 profile.loading 
                 ? <img className="d-block mx-auto my-4" src={LoadIcon} alt="loading" />
-                : <Info/>
+                : <Posts auth={auth} profile={profile} dispatch={dispatch} id={id}/>
             }
-            <Posts/>
+
         </div>
     )
 }

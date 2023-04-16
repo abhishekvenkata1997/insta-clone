591192199ef9564194c1d50e95d9587029274f18
Kasavaraju-v <114193191+Kasavaraju-v@users.noreply.github.com>
2023-04-15 21:35:33 -0400

    Added components for Posts and Status

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react'
+
+
+import { useSelector } from 'react-redux'
+import LoadIcon from './../images/loading.gif'
+
+import Status from './../components/home/Status'
+import Posts from './../components/home/Posts'
 
 const Home = () => {
+
     return (
-        <div>
-            <h1>Home!</h1>
+        <div className="home row mx-0">
+            <div className='col-md-8'>
+                <Status/>
+                <Posts/>
+            </div>
+
+            <div className='col-md-4'>
+            </div>
         </div>
     )
 }
-
 export default Home

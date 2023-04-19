ef612e0f9bd8d6861f675f6314e75c5454e2a3d5
Kasavaraju-v <114193191+Kasavaraju-v@users.noreply.github.com>
2023-04-19 16:28:37 -0400

    Refactored server.js file

--- server.js
+++ server.js
@@ -1,47 +1,48 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const authRouter = require('./routes/authRoutes')
-const SocketServer = require('./socketServer')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const authRouter = require("./routes/authRoutes");
+const SocketServer = require("./socketServer");
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
-
-const users = []
-io.on('connection', socket =>{
-    console.log(socket.id+"Connected!")
-    SocketServer(socket)
-})
+const users = [];
+io.on("connection", (socket) => {
+    console.log(socket.id + "Connected!");
+    SocketServer(socket);
+});
 
 //Routes
-app.use('/api',require('./routes/authRoutes'))
-app.use('/api',require('./routes/userRouter'))
-app.use('/api',require('./routes/postRoutes'))
-app.use('/api',require('./routes/commentRoutes'))
-app.use('/api', require('./routes/notifyRoutes'))
-const URI = process.env.MONGODB_URL
+app.use("/api", require("./routes/authRoutes"));
+app.use("/api", require("./routes/userRouter"));
+app.use("/api", require("./routes/postRoutes"));
+app.use("/api", require("./routes/commentRoutes"));
+app.use("/api", require("./routes/notifyRoutes"));
+app.use("/api", require("./routes/messageRouter"));
+
+const URI = process.env.MONGODB_URL;
 mongoose.connect(URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+    useUnifiedTopology: true,
+});
 
-mongoose.connection.on('connected',() => {
+mongoose.connection.on("connected", () => {
     console.log("Connected to the database");
 });
 
-mongoose.connection.on('error', err => {
-    console.log('Mongo DB Error in connection: '+err);
-})
+mongoose.connection.on("error", (err) => {
+    console.log("Mongo DB Error in connection: " + err);
+});
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 6000;
 http.listen(port, () => {
-    console.log("Server is running in port:"+port);
-})
+    console.log("Server is running in port:" + port);
+});

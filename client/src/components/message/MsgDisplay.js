fd20f40e01b91e8c39453f330c4f39a835edf190
Kasavaraju-v <114193191+Kasavaraju-v@users.noreply.github.com>
2023-04-19 11:47:09 -0400

    Updated message display functionality

--- client/src/components/message/MsgDisplay.js
+++ client/src/components/message/MsgDisplay.js
@@ -1,21 +1,60 @@
 import React from "react";
 import Avatar from "./../Avatar";
+import { imageShow, videoShow } from "./../../utils/mediaShow";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteMessages } from "../../redux/actions/messageAction";
 
-const MsgDisplay = ({ user }) => {
-  return (
-    <>
-      <div className="chat_title">
-        <Avatar src={user.avatar} size="small-avatar" />
-        <span>{user.username}</span>
-      </div>
-      <div className="chat_text">
-        Here is me claiming I am not okay but I do not know if its right or not,
-        but I am not sure if it works out or not.
-      </div>
+const MsgDisplay = ({ user, msg, theme, data }) => {
+    const { auth } = useSelector((state) => state);
+    const dispatch = useDispatch();
 
-      <div className="chat_time">April 2023</div>
-    </>
-  );
+    const handleDeleteMessages = () => {
+        if (!data) return;
+        if (window.confirm("Do you want to delete?")) {
+            dispatch(deleteMessages({ msg, data, auth }));
+            //console.log({ msg, data });
+        }
+    };
+    return (
+        <>
+            <div className="chat_title">
+                <Avatar src={user.avatar} size="small-avatar" />
+                <span>{user.username}</span>
+            </div>
+
+            <div className="you_content">
+                {user._id === auth.user._id && (
+                    <i
+                        className="fas fa-trash text-danger"
+                        onClick={handleDeleteMessages}
+                    />
+                )}
+
+                <div>
+                    {msg.text && (
+                        <div
+                            className="chat_text"
+                            style={{
+                                filter: theme ? "invert(1)" : "invert(0)",
+                            }}
+                        >
+                            {msg.text}
+                        </div>
+                    )}
+                    {msg.media.map((item, index) => (
+                        <div key={index}>
+                            {item.url.match(/video/i)
+                                ? videoShow(item.url, theme)
+                                : imageShow(item.url, theme)}
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="chat_time">
+                {new Date(msg.createdAt).toLocaleString()}
+            </div>
+        </>
+    );
 };
 
 export default MsgDisplay;

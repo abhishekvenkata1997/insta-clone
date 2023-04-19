d92255427ebda5efd96d27a76a6bbfad7bf291a1
Sreya-Vallabhaneni <114380178+Sreya-Vallabhaneni@users.noreply.github.com>
2023-04-19 03:01:08 -0400

    creted message reducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,28 +1,30 @@
-import { combineReducers } from 'redux'
-import auth from './authReducer'
-import alert from './alertReducer'
-import theme from './themeReducer'
-import profile from './profileReducer'
-import status from './statusReducer'
-import homePosts from './postReducer'
-import modal from './modalReducer'
-import detailPost from './detailPostReducer'
-import discover from './discoverReducer'
-import suggestions from './suggestionsReducer'
-import socket from './socketReducer'
-import notify from './notifytReducer'
+import { combineReducers } from "redux";
+import auth from "./authReducer";
+import alert from "./alertReducer";
+import theme from "./themeReducer";
+import profile from "./profileReducer";
+import status from "./statusReducer";
+import homePosts from "./postReducer";
+import modal from "./modalReducer";
+import detailPost from "./detailPostReducer";
+import discover from "./discoverReducer";
+import suggestions from "./suggestionsReducer";
+import socket from "./socketReducer";
+import notify from "./notifytReducer";
+import message from "./messageReducer";
 
 export default combineReducers({
-    auth,
-    alert,
-    theme,
-    profile,
-    status,
-    homePosts,
-    modal,
-    detailPost,
-    discover,
-    suggestions,
-    socket,
-    notify
-})
+  auth,
+  alert,
+  theme,
+  profile,
+  status,
+  homePosts,
+  modal,
+  detailPost,
+  discover,
+  suggestions,
+  socket,
+  notify,
+  message,
+});

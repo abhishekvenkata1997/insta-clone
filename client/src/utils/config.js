f3417876e9f743425f611a0ed7581c9fa5cccd03
Kasavaraju-v <114193191+Kasavaraju-v@users.noreply.github.com>
2023-04-20 14:07:43 -0400

    Updated config for heroku

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,3 +1,3 @@
 import { useResolvedPath } from "react-router"
 
-export const BASE_URL = 'http://insta-clone-webdesign.herokuapp.com:3000'
+export const BASE_URL = 'http://insta-clone-webdesign.herokuapp.com'
